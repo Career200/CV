0cc653e9a766ffe00fc77e178ade6d74cb08d94b
Career404 <105638831+Career404@users.noreply.github.com>
2023-01-27 13:27:32 +0400

    feat: horizontal scroll

--- script.js
+++ script.js
@@ -2,49 +2,54 @@
 // make cursor styles into classes and switch between them with JS
 
 // Custom cursor
-var cursor = document.getElementById('cursor');
-document.addEventListener('mousemove', function(e){
-    var x = e.pageX;
-    var y = e.pageY;
-    cursor.style.left = x + "px";
-    cursor.style.top = y + "px";
-});
+var cursor = document.getElementById('cursor')
+document.addEventListener('mousemove', function (e) {
+	var x = e.pageX
+	var y = e.pageY
+	cursor.style.left = x + 'px'
+	cursor.style.top = y + 'px'
+})
 
 //Drag to scroll img
-const slider = document.querySelector('.image-container');
-let isDown = false;
-let startX;
-let scrollLeft;
+const slider = document.querySelector('.image-container')
+let isDown = false
+let startX
+let scrollLeft
 
-slider.addEventListener("mousedown", e => {
-    isDown = true;
-    slider.classList.add("active");
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-    isDown = false;
-    slider.classList.remove("active");
-})  ;
-slider.addEventListener("mouseup", () => {
-    isDown = false;
-    slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", e => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = x - startX;
-    slider.scrollLeft = scrollLeft - walk;
-});
+slider.addEventListener('mousedown', (e) => {
+	isDown = true
+	slider.classList.add('active')
+	startX = e.pageX - slider.offsetLeft
+	scrollLeft = slider.scrollLeft
+})
+slider.addEventListener('mouseleave', () => {
+	isDown = false
+	slider.classList.remove('active')
+})
+slider.addEventListener('mouseup', () => {
+	isDown = false
+	slider.classList.remove('active')
+})
+slider.addEventListener('mousemove', (e) => {
+	if (!isDown) return
+	e.preventDefault()
+	const x = e.pageX - slider.offsetLeft
+	const walk = x - startX
+	slider.scrollLeft = scrollLeft - walk
+})
+slider.addEventListener('wheel', (event) => {
+	event.preventDefault()
+	var delta = event.deltaX || event.deltaY
+	slider.scrollLeft += delta * 1.5
+})
 
 // Click anywhere to close .seemore
 const clickTarget = document.getElementById('label')
 const checkbox = document.getElementById('lifestory')
 const seemorebox = document.querySelector('.seemore')
 
-document.addEventListener("click", (e) => {
-    if (checkbox.checked && e.target.contains(seemorebox || clickTarget)) {
-        checkbox.checked = false;
-    }
+document.addEventListener('click', (e) => {
+	if (checkbox.checked && e.target.contains(seemorebox || clickTarget)) {
+		checkbox.checked = false
+	}
 })
